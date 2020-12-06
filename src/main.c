eafa0e83f41567860e73c74495e4bc8956f69bff
99003180 <75408384+99003180@users.noreply.github.com>
2020-12-06 15:08:08 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
-#include "calculator.h"
+#include<calculator.h>
+#include <stdio.h>
+#include<math.h>
+//#include <stdlib.h>
 
-int main(void)
+int main()
 {
-	int sum(int a, int b);
-	return 0;
+    int ch;
+    printf("Enter your choice\n");
+    printf(" 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n 5.Reverse\n 6.Decimal to Binary\n 7.Matrix Addition\n 8.Inverse\n 9.Square Root");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+        case 1: add();
+                break;
+        case 2: sub();
+                break;
+        case 3: mult();
+                break;
+        case 4: div();
+                break;
+        case 5: rev();
+                break;
+        case 6: DecToBin();
+                break;
+        case 7: matrix_sum();
+                break;
+        case 8: inv();
+                break;
+        case 9 :square();
+                break;
+    }
+    return 0;
 }
